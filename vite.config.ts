afe9f7af49a653acd963e09bb6511bc07a5bf88e
acbserver <37807640+acbserver@users.noreply.github.com>
2025-03-01 20:26:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './', // Asegura que Render sirva correctamente los archivos estáticos
 })
